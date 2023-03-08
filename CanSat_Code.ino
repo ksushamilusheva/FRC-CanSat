6eaf8055529be8235f86a66c91536f1b00c48217
ksushamilusheva <109567903+ksushamilusheva@users.noreply.github.com>
2023-03-08 13:27:55 -0600

    Update CanSat_Code.ino

--- CanSat_Code.ino
+++ CanSat_Code.ino
@@ -17,18 +17,35 @@
 #include <Adafruit_MMA8451.h>
 #include <Adafruit_Sensor.h>
 
+File data;
+
+Adafruit_BMP280 bmp;
+Adafruit_MMA8451 mma = Adafruit_MMA8451();
+
 void setup() 
 {
   Serial.begin(9600);
-  data = SD.open("lloydSensorCode", FILE_WRITE);
 
-  data.println(); //BMP280
+//BMP280
+unsigned status;
+status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
   data.println(); //MMA8451
 
   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  data = SD.open("SensorCode", FILE_WRITE);
 
+  temp = bmp.readTemperature(); //in Celcius
+  pressure = bmp.readPressure(); //in Pascals
+  altitude = bmp.readAltitude(1013.25); //in meters
+  sensors_event_t event = mma.getEvent(&event);
+  accel = event.acceleration.z(); //in meters/second^2
 }
